4f85f6f5d2fbfe40f01bd8ad1b69630a86c86894
Eugene The Red <me@eugenethered.com>
2022-01-25 11:24:51 +0000

    added default for json transformation

---
+++ tests/utility/json_utility_test.py
@@ -0,0 +1,34 @@
+import unittest
+
+from utility.json_utility import as_json, as_data
+
+
+class JsonUtilityTestCase(unittest.TestCase):
+
+    def test_should_load_json_data(self):
+        json_string = '{"instrument": "BTCTRY","ignore": true}'
+        json_data = as_json(json_string)
+        self.assertEqual('BTCTRY', json_data['instrument'])
+        self.assertEqual(True, json_data['ignore'])
+
+    def test_should_obtain_json_to_data(self):
+        json_string = '{"instrument": "BTCTRY","ignore": true}'
+        json_data = as_json(json_string)
+        self.assertEqual('BTCTRY', as_data(json_data, 'instrument'))
+        self.assertEqual(True, as_data(json_data, 'ignore'))
+
+    def test_should_obtain_no_json_to_data_when_data_in_json_does_not_exist(self):
+        json_string = '{"instrument": "BTCTRY","ignore": true}'
+        json_data = as_json(json_string)
+        self.assertEqual(None, as_data(json_data, 'SomethingNotThere'))
+
+    def test_should_obtain_default_data_when_data_in_json_does_not_exist(self):
+        json_string = '{"instrument": "BTCTRY"}'
+        json_data = as_json(json_string)
+        self.assertEqual(None, as_data(json_data, 'SomethingNotThere'), 'Should use default None')
+        self.assertEqual(False, as_data(json_data, 'SomethingNotThere', default=False), 'Should override default from None to False')
+        self.assertEqual(True, as_data(json_data, 'SomethingNotThere', default=True), 'Should override default from None to True')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utility/json_utility.py
+++ utility/json_utility.py
@@ -9,7 +9,7 @@ def as_pretty_json(json_data):
     return json.dumps(json_data, indent=4, sort_keys=True)
 
 
-def as_data(data, key):
+def as_data(data, key, default=None):
     if key in data:
         return data[key]
-    return None
+    return default
