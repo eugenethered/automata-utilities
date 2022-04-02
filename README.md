ce4445c90459daa5ad297d18dc3ff537d9627153
Eugene The Red <me@eugenethered.com>
2022-04-02 17:35:41 +0100

    changing package name + adding license

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
 # utilities
-Utilities 
+Automata Utilities 
 
 ## Packaging
 `python3 -m build`
 
 ## Clean
-`rm -fr dist utilities.egg-info`
-
-## Requirements
-`pip install build`
-
-## Distributing
-After building, see `dist` directory. (Install tar)
+`rm -fr dist automata.utilities.egg-info`
--- tests/utility/json_utility_test.py
+++ tests/utility/json_utility_test.py
@@ -6,24 +6,24 @@ from utility.json_utility import as_json, as_data
 class JsonUtilityTestCase(unittest.TestCase):
 
     def test_should_load_json_data(self):
-        json_string = '{"instrument": "BTCTRY","ignore": true}'
+        json_string = '{"instrument": "CASH","ignore": true}'
         json_data = as_json(json_string)
-        self.assertEqual('BTCTRY', json_data['instrument'])
+        self.assertEqual('CASH', json_data['instrument'])
         self.assertEqual(True, json_data['ignore'])
 
     def test_should_obtain_json_to_data(self):
-        json_string = '{"instrument": "BTCTRY","ignore": true}'
+        json_string = '{"instrument": "CASH","ignore": true}'
         json_data = as_json(json_string)
-        self.assertEqual('BTCTRY', as_data(json_data, 'instrument'))
+        self.assertEqual('CASH', as_data(json_data, 'instrument'))
         self.assertEqual(True, as_data(json_data, 'ignore'))
 
     def test_should_obtain_no_json_to_data_when_data_in_json_does_not_exist(self):
-        json_string = '{"instrument": "BTCTRY","ignore": true}'
+        json_string = '{"instrument": "CASH","ignore": true}'
         json_data = as_json(json_string)
         self.assertEqual(None, as_data(json_data, 'SomethingNotThere'))
 
     def test_should_obtain_default_data_when_data_in_json_does_not_exist(self):
-        json_string = '{"instrument": "BTCTRY"}'
+        json_string = '{"instrument": "CASH"}'
         json_data = as_json(json_string)
         self.assertEqual(None, as_data(json_data, 'SomethingNotThere'), 'Should use default None')
         self.assertEqual(False, as_data(json_data, 'SomethingNotThere', default=False), 'Should override default from None to False')
