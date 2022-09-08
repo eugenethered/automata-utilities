7959f3c971cf98727c5dbccb5c96b1517a76d23b
Eugene The Red <me@eugenethered.com>
2022-09-08 22:43:24 +0100

    ensuring nano-ish conversion works

--- tests/coreutility/date/NanoTimestamp_test.py
+++ tests/coreutility/date/NanoTimestamp_test.py
@@ -16,6 +16,12 @@ class NanoTimestampTestCase(unittest.TestCase):
         expected_datetime = datetime(2022, 9, 6, 14, 11, 24, 448608)
         self.assertEqual(actual_datetime, expected_datetime)
 
+    def test_should_obtain_datetime_from_nanoseconds_zeroed(self):
+        nanoseconds = 1662473484448608000
+        actual_datetime = NanoTimestamp.as_datetime(nanoseconds)
+        expected_datetime = datetime(2022, 9, 6, 14, 11, 24, 448608)
+        self.assertEqual(actual_datetime, expected_datetime)
+
     def test_should_obtain_nanoseconds_from_datetime(self):
         # python cannot yet handle nano time (therefore-ish)
         nano_ish_datetime = datetime(2022, 9, 6, 14, 11, 24, 448608)
